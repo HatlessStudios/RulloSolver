c0edfe91e1dc30bd9863bb59eede177a075f8171
ScottRWood <productionspulsar@gmail.com>
2018-04-07 14:40:34 +0100

    Begin GUI implementation

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,51 @@
-public class GUI {
+import java.awt.*;
+import javax.swing.*;
+
+public class GUI extends JFrame{
+
+    private Label lblCount;
+    JPanel solution;
+
+    GUI(int m, int n, Node[][] matrix){
+
+        //Creates window with solution heading and with solution pane
+        setLayout(new FlowLayout());
+        lblCount = new Label("Solution");
+        add(lblCount);
+
+        //Set window properties
+        setTitle("Rullo Solution");
+        setSize(1000, 1000);
+        setLocation(250, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+
+        //Generate solution panel and pack into window
+        panel(m, n, matrix);
+        this.getContentPane().add(solution, BorderLayout.CENTER);
+        this.pack();
+        setSize(1000, 1000);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        GUI app = new GUI(10, 10);
+    }
+
+    /**
+     * Creates the panel in the grid layout
+     * @param m, n The dimensions of the grid
+     */
+    void panel(int m, int n, Node[][] matrix){
+
+        solution = new JPanel();
+        solution.setLayout(new GridLayout(m,n));
+
+        for (int i = 0; i <= n; i++){
+            for (int j = 0; j <= m; j++){
+                add(new CircleLabel(matrix[j][i]));
+            }
+        }
+    }
 }
