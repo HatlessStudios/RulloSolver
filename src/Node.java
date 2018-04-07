397fbbc3ea00823ffcb928ea7d934f06026e9aa7
FaustsDaemon <chris.phippen98@gmail.com>
2018-04-07 13:43:59 +0100

    Initial Commit

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,16 @@
-public class Node {
+class Node {
+
+    private int value;
+    private boolean state;
+    private boolean lock;
+
+    Node(int value){
+
+         this.value = value;
+         state = true;
+         lock = false;
+
+    }
+
+    int get()
 }
