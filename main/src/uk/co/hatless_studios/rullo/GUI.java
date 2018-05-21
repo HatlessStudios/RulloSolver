73aff3b5da7ab91f0e37899c3982a079678b3a92
ScottRWood <productionspulsar@gmail.com>
2018-05-21 13:26:11 +0100

    Update GUI

--- main/src/uk/co/hatless_studios/rullo/GUI.java
+++ main/src/uk/co/hatless_studios/rullo/GUI.java
@@ -9,7 +9,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Label;
 
 
@@ -21,11 +25,12 @@ public class GUI extends JFrame {
     GUI(int m, int n, Node[][] matrix){
 
         //Creates window with solution heading and with solution pane
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout(0, 10));
         this.getContentPane().setBackground(Color.BLACK);
-        this.getContentPane().setForeground(Color.WHITE);
         lblCount = new Label("Solution");
-        add(lblCount);
+        lblCount.setAlignment(Label.CENTER);
+        lblCount.setForeground(Color.WHITE);
+        lblCount.setFont(new Font("Serif", Font.BOLD, 36));
 
         //Set window properties
         setTitle("Rullo Solution");
@@ -39,7 +44,6 @@ public class GUI extends JFrame {
         panel(m, n, matrix);
         this.getContentPane().add(solution, BorderLayout.CENTER);
         this.pack();
-        setSize(1000, 1000);
         setVisible(true);
     }
 
@@ -54,12 +58,18 @@ public class GUI extends JFrame {
     void panel(int m, int n, Node[][] matrix){
 
         solution = new JPanel();
-        solution.setLayout(new GridLayout(m,n));
+        solution.setLayout(new GridBagLayout());
         solution.setBackground(Color.BLACK);
 
+        GridBagConstraints constraints = new GridBagConstraints(0, 0, n, 1, 0D, 0D, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(10, 10, 10, 10), 10, 10);
+        solution.add(lblCount, constraints);
+
+        constraints.gridwidth = 1;
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
-                solution.add(new CircleLabel(" " + Integer.toString(matrix[j][i].getValue()), matrix[j][i].getState() ? Color.YELLOW : Color.WHITE, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50)));
+                constraints.gridx = i;
+                constraints.gridy = j + 1;
+                solution.add(new CircleLabel(Integer.toString(matrix[j][i].getValue()), matrix[j][i].getState() ? Color.YELLOW : Color.WHITE, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50)), constraints);
             }
         }
     }
