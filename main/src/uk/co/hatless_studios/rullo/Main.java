5746b5f785c34e9a76d389f1c72da479085261b8
Alexandre Tunstall <alexandrejamestunstall@gmail.com>
2018-05-22 12:22:33 +0100

    Add input validation to table

--- main/src/uk/co/hatless_studios/rullo/Main.java
+++ main/src/uk/co/hatless_studios/rullo/Main.java
@@ -10,8 +10,7 @@ import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
-import javax.swing.table.DefaultTableModel;
-import java.awt.BorderLayout;
+import javax.swing.table.AbstractTableModel;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -20,7 +19,7 @@ import java.awt.event.KeyListener;
 
 public class Main implements KeyListener {
     private static final Insets insets = new Insets(5, 5, 5, 5);
-    private static Node[][] matrix;
+
     private final Object lock;
     private JFrame frame;
     private JPanel dimPanel;
@@ -33,14 +32,14 @@ public class Main implements KeyListener {
     private JPanel tblPanel;
     private JScrollPane tblPane;
     private JTable nodeTable;
-    private DefaultTableModel tableModel;
+    private MatrixModel tableModel;
     private JButton backButton;
     private JButton startButton;
+    private JPanel dummyPanel;
 
     public static void main(String[] args) {
         System.out.println("Rullo solver is running...");
         Main main = new Main();
-        int m, n;
         synchronized (main.lock) {
             main.initializeDim();
             main.showDim();
@@ -61,8 +60,6 @@ public class Main implements KeyListener {
                     matrix[i][j] = new Node(0);
             }
         }*/
-
-        // TODO Forward matrix to GUI
     }
 
     private Main() {
@@ -104,15 +101,34 @@ public class Main implements KeyListener {
     }
 
     private void initializeTbl() {
-        tblPanel = new JPanel(new BorderLayout());
-        nodeTable = new JTable(tableModel = new DefaultTableModel());
+        tblPanel = new JPanel(new GridBagLayout());
+        nodeTable = new JTable(tableModel = new MatrixModel());
         tblPane = new JScrollPane(nodeTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        tblPanel.add(tblPane, BorderLayout.CENTER);
+        backButton = new JButton("Back");
+        startButton = new JButton("Start");
+        dummyPanel = new JPanel();
+        GridBagConstraints constraints = new GridBagConstraints(0, 0, 3, 1,
+                1D, 1D, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
+        tblPanel.add(tblPane, constraints);
+        constraints.gridy++;
+        constraints.gridwidth = 1;
+        constraints.weighty = constraints.weightx = 0D;
+        tblPanel.add(backButton, constraints);
+        constraints.gridx++;
+        constraints.weightx = 1D;
+        tblPanel.add(dummyPanel, constraints);
+        constraints.gridx++;
+        constraints.gridwidth = 1;
+        constraints.weightx = 0D;
+        tblPanel.add(startButton, constraints);
         nodeTable.setTableHeader(null);
         nodeTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        backButton.addActionListener(event -> showDim());
+        startButton.addActionListener(event -> onStart());
     }
 
     private void showDim() {
+        if (tblPanel != null) frame.remove(tblPanel);
         frame.add(dimPanel);
         frame.setResizable(false);
         frame.setSize(160, 140);
@@ -122,8 +138,7 @@ public class Main implements KeyListener {
     }
 
     private void showTbl(int m, int n) {
-        tableModel.setRowCount(m);
-        tableModel.setColumnCount(n);
+        tableModel.setSize(m, n);
         frame.remove(dimPanel);
         frame.add(tblPanel);
         frame.setResizable(true);
@@ -133,14 +148,19 @@ public class Main implements KeyListener {
     }
 
     private void onNext() {
+        if (widthField.getText().length() == 0 || heightField.getText().length() == 0) return;
         synchronized (lock) {
             showTbl(Integer.parseInt(heightField.getText()), Integer.parseInt(widthField.getText()));
         }
     }
 
+    private void onStart() {
+        // TODO Forward matrix to GUI
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
-        if (e.getComponent() instanceof JTextField &&e.getKeyChar() < '0' || e.getKeyChar() > '9') {
+        if (e.getComponent() instanceof JTextField && e.getKeyChar() < '0' || e.getKeyChar() > '9' || ((JTextField) e.getComponent()).getText().length() >= 3) {
             e.consume();
         }
     }
@@ -149,11 +169,13 @@ public class Main implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_ENTER:
-                if (frame.getFocusOwner() != cancelButton) {
+                if (frame.getFocusOwner() instanceof JButton) {
+                    ((JButton) frame.getFocusOwner()).doClick();
+                } else {
                     onNext();
                     e.consume();
-                    break;
                 }
+                break;
             case KeyEvent.VK_ESCAPE:
                 System.exit(0);
         }
@@ -161,4 +183,54 @@ public class Main implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {}
+
+    private class MatrixModel extends AbstractTableModel {
+        private Node[][] nodes;
+        private int rows;
+        private int columns;
+
+        private MatrixModel() {
+            setSize(0, 0);
+        }
+
+        @Override
+        public int getRowCount() {
+            return rows;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return columns;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return nodes[rowIndex][columnIndex].getValue();
+        }
+
+        @Override
+        public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+            nodes[rowIndex][columnIndex] = new Node((int)aValue);
+            fireTableCellUpdated(rowIndex, columnIndex);
+        }
+
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return true;
+        }
+
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            return Integer.class;
+        }
+
+        private void setSize(int rows, int columns) {
+            //int oldRows = this.rows, oldColumns = this.columns;
+            this.rows = rows;
+            this.columns = columns;
+            nodes = new Node[rows][columns];
+            for (int r = 0; r < rows; r++) for (int c = 0; c < columns; c++) nodes[r][c] = new Node(0);
+            fireTableStructureChanged();
+        }
+    }
 }
