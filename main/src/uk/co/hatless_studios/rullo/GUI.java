0c9e1b3978fcaec638de0cb0d5cf998ecfbc8c37
ScottRWood <productionspulsar@gmail.com>
2018-05-22 13:08:44 +0100

    Fixed scaling for UI so different length strings have same font size

--- main/src/uk/co/hatless_studios/rullo/GUI.java
+++ main/src/uk/co/hatless_studios/rullo/GUI.java
@@ -56,6 +56,7 @@ public class GUI extends JFrame {
      */
     void createPanel(int m, int n, Node[][] matrix, int[] rowSums, int[] columnSums){
 
+        //Create panel and set layout and properties
         solution = new JPanel();
         solution.setLayout(new GridBagLayout());
         solution.setBackground(Color.BLACK);
@@ -64,21 +65,45 @@ public class GUI extends JFrame {
         solution.add(lblCount, constraints);
 
         constraints.gridwidth = 1;
+
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (Integer.toString(matrix[j][i].getValue()).length() > maxLength)
+                    maxLength = Integer.toString(matrix[j][i].getValue()).length();
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            if (Integer.toString(columnSums[i]).length() > maxLength)
+                maxLength = Integer.toString(columnSums[i]).length();
+        }
+
+        for (int j = 0; j < m; j++){
+            if (Integer.toString(rowSums[j]).length() > maxLength)
+                maxLength = Integer.toString(rowSums[j]).length();
+        }
+
+        //Insert each row and column
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
                 constraints.gridx = i;
                 constraints.gridy = j + 1;
-                solution.add(new CircleLabel(Integer.toString(matrix[j][i].getValue()), matrix[j][i].getState() ? Color.GREEN : Color.RED, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50)), constraints);
+                solution.add(new CircleLabel(Integer.toString(matrix[j][i].getValue()), matrix[j][i].getState() ? Color.GREEN : Color.RED, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50), maxLength), constraints);
             }
+
+            //Add column sum to end of column
             constraints.gridx = i;
             constraints.gridy = m + 1;
-            solution.add(new CircleLabel(Integer.toString(columnSums[i]), Color.YELLOW, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50)), constraints);
+            solution.add(new CircleLabel(Integer.toString(columnSums[i]), Color.YELLOW, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50), maxLength), constraints);
         }
 
+        //Add row sums to end of rows
         for (int j = 0; j < m; j++){
             constraints.gridx = n;
             constraints.gridy = j + 1;
-            solution.add(new CircleLabel(Integer.toString(rowSums[j]), Color.YELLOW, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50)), constraints);
+            solution.add(new CircleLabel(Integer.toString(rowSums[j]), Color.YELLOW, new Dimension((this.getWidth()/m) - 50, (this.getHeight()/n) - 50), maxLength), constraints);
         }
     }
 }
